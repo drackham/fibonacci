1ed1e80c5a01a907e7c38c811925ef0742620aec
drackham <ddrackham@me.com>
2017-03-27 17:31:31 -0600

    Added two tests. The first tests if the index page redirection. The second tests the API when no sim box is provided.

--- jarvis-mcmc_api/jarvis-mcmc_test.py
+++ jarvis-mcmc_api/jarvis-mcmc_test.py
@@ -16,3 +16,22 @@ def test_jarvis_mcmc_docs():
     """
     resp = test_app.get('/jarvis_mcmc/docs')
     eq_(resp.status_code, STATUS_REDIR, "Response code %s is not != %s for /jarvis-mcmc/docs redirection" % (resp.status_code, STATUS_REDIR))
+
+def test_index():
+    """
+    function: test_index
+    notes: Ensure that the index redirects to the documentation
+    """
+    resp = test_app.get('/')
+    page = "http://localhost/static/index.html"
+    
+    eq_(resp.status_code, STATUS_REDIR, "Response code %s != %s for / redirection" % (resp.status_code, STATUS_REDIR))
+    eq_(resp.location, page, "Page URL %s is not %s" % (resp.location, page))
+
+def test_sim_no_box_api():
+    """
+    function: test_sim_no_box_api
+    notes: Ensure the API responds correctly when no sim box is specified
+    """
+    resp = test_app.get('/jarvis_mcmc/sim/')
+    eq_(resp.status_code, STATUS_CLIENT_ERROR, "Response code %s != %s for /jarvis_mcmc/sim" % (resp.status_code, STATUS_CLIENT_ERROR))
